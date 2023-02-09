808c4bb9d41ecb7ffdaaca126ac7f2dd51bb0da3
TeymurazGuzoev <dehbno4b@gmail.com>
2023-02-10 01:27:06 +0300

    add first strike to DB

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/jackc/pgx/stdlib"
 )
 
+var dsn string = "postgres://postgres:917836@localhost:5432/lightning?"
 var filename string = "./public/Repr.2022.07.03.txt"
 
 func main() {
@@ -11,8 +15,21 @@ func main() {
 	dr := dataReader{}
 	data := dr.readFromFile(filename)
 
-	fmt.Printf("%#v", data[0])
-	fmt.Println()
+	// fmt.Printf("%#v", data[5000])
+	// fmt.Println()
+	// fmt.Printf("%#v", data[5000].time)
+	// fmt.Println()
 	fmt.Println("datalen:", len(data))
 
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close() // (3)
+
+	query := `INSERT INTO strikes (time,latitude,longitude)
+	values($1,$2,$3)`
+	result, err := db.Exec(query, data[0].time, data[0].latitude, data[0].longitude)
+	fmt.Printf("%#v", result)
+	fmt.Printf("%#v", err)
 }
