45cb47382e4a10c8fbc2c402248910dbcc4f4e5b
Teymuraz <dehbno4b@gmail.com>
2023-02-10 20:46:58 +0300

    add creat tab function

--- main.go
+++ main.go
@@ -9,27 +9,40 @@ import (
 
 var dsn string = "postgres://postgres:917836@localhost:5432/lightning?"
 var filename string = "./public/Repr.2022.07.03.txt"
+var queryInsert string = `INSERT INTO strikes (time,latitude,longitude,signal,cloud) 
+values($1,$2,$3,$4,$5)`
+var queryMakeTab = ` drop table if exists strikes;
+    create table if not exists strikes(
+        id serial primary key,
+        time timestamptz,
+        latitude numeric(6,4),
+        longitude numeric(6,4),
+        signal smallint,
+		cloud boolean,
+		cluster integer
+    );`
 
 func main() {
 	//получаем слайс молниевых структур
 	dr := dataReader{}
 	data := dr.readFromFile(filename)
-
-	// fmt.Printf("%#v", data[5000])
-	// fmt.Println()
-	// fmt.Printf("%#v", data[5000].time)
-	// fmt.Println()
 	fmt.Println("datalen:", len(data))
 
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer db.Close() // (3)
+	defer db.Close()
 
-	query := `INSERT INTO strikes (time,latitude,longitude)
-	values($1,$2,$3)`
-	result, err := db.Exec(query, data[0].time, data[0].latitude, data[0].longitude)
-	fmt.Printf("%#v", result)
-	fmt.Printf("%#v", err)
+	resp, err := db.Exec(queryMakeTab)
+	if err != nil {
+		fmt.Println("query make tab err:", err)
+	}
+	fmt.Println("query make tab resp:", resp)
+	for i, _ := range data {
+		_, err := db.Exec(queryInsert, data[i].time, data[i].latitude, data[i].longitude, data[i].signal, data[i].cloud)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
